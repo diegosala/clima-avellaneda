e7197ac0788e886aacdf3627dba6aec996cb2e42
Diego Sala <diego45@gmail.com>
2013-11-28 14:52:38 -0300

    Migrations para tablas base

---
+++ app/database/migrations/2013_11_28_172345_create_live_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLiveTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('live', function($table) {
+			$table->bigIncrements('id')->unsigned();
+			$table->dateTime('timestamp');
+			$table->decimal('temperature',4,2);
+			$table->integer('humidity')->unsigned();
+			$table->integer('wind_speed')->unsigned();
+			$table->integer('wind_gust')->unsigned();
+			$table->smallInteger('wind_direction')->unsigned();
+			$table->smallInteger('rain')->unsigned();
+			$table->decimal('battery',5,3)->unsigned();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('live');
+	}
+
+}
---
+++ app/database/migrations/2013_11_28_173658_create_records_table.php
@@ -0,0 +1,78 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRecordsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('years', function($table) {
+			$table->bigIncrements('id')->unsigned();
+			$table->decimal('max_temperature',5,2);
+			$table->dateTime('max_temperaure_date');
+			$table->decimal('min_temperature',5,2);
+                        $table->dateTime('min_temperaure_date');
+			$table->decimal('avg_temperature',5,2);
+			$table->decimal('avg_humidity',3,2)->unsigned();
+			$table->integer('max_windgust')->unsigned();
+			$table->dateTime('max_windgust_date');
+			$table->decimal('avg_windspeed',3,1);
+			$table->decimal('sum_rain',6,1);
+		});
+
+		Schema::create('months', function($table) {
+                        $table->bigIncrements('id')->unsigned();
+			$table->bigInteger('year_id')->unsigned();
+			$table->smallInteger('month')->unsigned();
+                        $table->decimal('max_temperature',5,2);
+                        $table->dateTime('max_temperaure_date');
+                        $table->decimal('min_temperature',5,2);
+                        $table->dateTime('min_temperaure_date');
+                        $table->decimal('avg_temperature',5,2);
+                        $table->decimal('avg_humidity',3,2)->unsigned();
+                        $table->integer('max_windgust')->unsigned();
+                        $table->dateTime('max_windgust_date');
+                        $table->decimal('avg_windspeed',3,1);
+                        $table->decimal('sum_rain',6,1);
+
+			$table->foreign('year_id')->references('id')->on('years');
+                });
+		
+		Schema::create('days', function($table) {
+                        $table->bigIncrements('id')->unsigned();
+                        $table->bigInteger('month_id')->unsigned();
+                        $table->date('date');
+                        $table->decimal('max_temperature',5,2);
+                        $table->time('max_temperaure_time',5,2);
+                        $table->decimal('min_temperature',5,2);
+                        $table->time('min_temperaure_time');
+                        $table->decimal('avg_temperature',5,2);
+                        $table->decimal('avg_humidity',3,2)->unsigned();
+                        $table->integer('max_windgust')->unsigned();
+                        $table->time('max_windgust_time');
+                        $table->decimal('avg_windspeed',3,1);
+                        $table->decimal('sum_rain',6,1);
+
+                        $table->foreign('month_id')->references('id')->on('months');
+                });
+
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('years');
+		Schema::drop('months');
+		Schema::drop('days');
+	}
+
+}
